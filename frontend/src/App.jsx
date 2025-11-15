b37b3c5c10618eddf86987f3978fbd906d701acb
BaraaZino <152164051+BaraaZino@users.noreply.github.com>
2025-11-15 17:16:47 +0300

    Base

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+/* this is the master router wiring every page and role together */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+   1
+  )
+}
+
+export default App
